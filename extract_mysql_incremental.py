ceb725ff99078f5569e1fd62f548052c5e4bb2e0
Lupercio421 <daniel.llupercio@gmail.com>
2022-10-01 18:32:34 -0400

    this one is scratching my head. aws_creds is not clear

--- extract_mysql_incremental.py
+++ extract_mysql_incremental.py
@@ -5,8 +5,14 @@
 # Here is the code to connect and query the Redshift cluster to get the MAX(LastUpdated) value from 
 # Orders table:
 
+from distutils.command.config import config
+from os import access
+import pymysql
+import csv
+import boto3
 import configparser
 import psycopg2
+from yaml import parse
 
 # get db Redshift connection info
 
@@ -22,10 +28,10 @@ port = parser.get("aws_creds", "port")
 
 rs_conn = psycopg2.connect(
     "dbname=" + dbname
-    + " user=" + user
-    + " password=" + password
-    + " host=" + host
-    + " port=" + port
+    + " user = " + user
+    + " password = " + password
+    + " host = " + host
+    + " port = " + port
 )
 
 rs_sql = """SELECT COALESCE(MAX(LastUpdated), '1900-01-01') FROM Orders;"""
@@ -40,4 +46,56 @@ last_updated_warehouse = result[0]
 rs_cursor.close()
 rs_conn.commit()
 
-print('WOrking?')
+# print('WOrking?')
+
+#get the MySQL connection info and connect
+parser = configparser.ConfigParser()
+parser.read("pipeline.conf")
+hostname = parser.get("mysql_config", "hostname")
+port = parser.get("mysql_config", "port")
+username = parser.get("mysql_config", "username")
+dbname = parser.get("mysql_config", "database")
+password = parser.get("mysql_config", "password")
+
+conn = pymysql.connect(host = hostname, 
+        user = username, 
+        password = password, 
+        db = dbname, 
+        port = int(port))
+
+if conn is None:
+    print("Error connecting to the MySQL database")    
+else:
+    print("MySQL connection established!")
+
+m_query = """SELECT * FROM Orders WHERE LastUpdated > %s;"""
+
+local_filename = "order_extract.csv"
+
+m_cursor = conn.cursor()
+m_cursor.execute(m_query, (last_updated_warehouse,))
+results = m_cursor.fetchall()
+
+with open(local_filename, 'w') as fp:
+    csv_w = csv.writer(fp, delimiter= "|")
+    csv_w.writerows(results)
+
+fp.close()
+m_cursor.close()
+conn.close()
+
+# load the aws_boto_credentials values
+parser = configparser.ConfigParser()
+parser.read("pipeline.conf")
+access_key = parser.get(
+    "aws_boto_credentials",
+    "access_key"
+)
+secret_key = parser.get("aws_boto_credentials","bucket_name")
+bucket_name = parser.get("aws_boto_credentials","bucket_name")
+
+s3 = boto3.client('s3', aws_access_key_id = access_key, aws_secret_access_key = secret_key)
+
+s3_file = local_filename
+
+s3.upload_file(local_filename, bucket_name, s3_file)
