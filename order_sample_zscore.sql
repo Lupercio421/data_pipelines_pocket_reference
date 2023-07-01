918759b85a7071975e70c4bebb48773ad30c9a27
Lupercio421 <daniel.llupercio@gmail.com>
2023-07-01 17:11:27 -0400

    more sql files from chapter 8

---
+++ order_sample_zscore.sql
@@ -0,0 +1,15 @@
+-- Active: 1685384646340@@danny-dppr-pg.cqxcxanvhef4.us-east-1.rds.amazonaws.com@5432@airflowdb@public
+WITH order_count_zscore AS (
+    SELECT
+        order_date,
+        order_count,
+        (order_count - avg(order_count) over ()) / (stddev(order_count) over ()) as z_score
+    FROM
+        orders_by_day
+)
+SELECT
+    ABS(z_score) AS twosided_score
+FROM
+    order_count_zscore
+WHERE
+    order_date = CAST('2020-10-05' AS DATE) - interval '1 day';
---
+++ zscore_90_twosided.sql
@@ -0,0 +1,2 @@
+-- Active: 1685384646340@@danny-dppr-pg.cqxcxanvhef4.us-east-1.rds.amazonaws.com@5432@airflowdb@public
+SELECT 1.645;
