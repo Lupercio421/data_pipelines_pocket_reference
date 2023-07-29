d03daf78ad135be23c46f9cf2a7a7505d30a3f98
Lupercio421 <daniel.llupercio@gmail.com>
2023-07-29 17:06:41 -0400

    adding services connection scripts to gitignore

--- validation_run_history.sql
+++
@@ -1,8 +0,0 @@
--- Active: 1685384646340@@danny-dppr-pg.cqxcxanvhef4.us-east-1.rds.amazonaws.com@5432@airflowdb@public
-CREATE TABLE validation_run_history (
-  script_1 varchar(255),
-  script_2 varchar(255),
-  comp_operator varchar(10),
-  test_result varchar(20),
-  test_run_at timestamp
-);
