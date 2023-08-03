5b2b880512b625fdade6ea59fb35d1416faa5cc8
Lupercio421 <daniel.llupercio@gmail.com>
2023-08-02 22:56:47 -0400

    better format for sql file

--- dag_history_daily_select.sql
+++ dag_history_daily_select.sql
@@ -1,4 +1,9 @@
-select 
-dag_id, 
-SUM(CASE WHEN dag_state = 'success' THEN 1 ELSE 0 END) / CAST(SUM(dag_run_count) AS DECIMAL(6,2))
-AS success_rate from dag_history_daily GROUP BY dag_id;
+select
+    dag_id,
+    SUM(
+        CASE WHEN dag_state = 'success' THEN 1 ELSE 0 END
+    ) / CAST(SUM(dag_run_count) AS DECIMAL(6, 2)) AS success_rate
+from
+    dag_history_daily
+GROUP BY
+    dag_id;
